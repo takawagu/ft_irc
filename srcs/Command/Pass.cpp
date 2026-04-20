ecb8ebd3b986ac6b955b1eb0a497a9ff510f61d5
s2108tomoka <s2108.ochitomoka@gmail.com>
2026-04-20 11:42:58 +0900

    add Pass Command

--- srcs/Command/Pass.cpp
+++ srcs/Command/Pass.cpp
@@ -6,7 +6,6 @@
 void Pass::executeAction(Server& server, Client& client, int fd)
 {
 	(void)fd;
-	(void)server;
 
 	if (params().size() <= 1)
 	{
@@ -14,15 +13,15 @@ void Pass::executeAction(Server& server, Client& client, int fd)
 		client.appendToSendBuffer(error_msg);
 		return;
 	}
-	std::cout << "Received PASS command with params: " << params()[1] << std::endl;
-	// if (params == server.config().password())
-	// {
-	// 	client.setRegistered(true);
-	// }
-	// else
-	// {
-	// 	std::string error_msg = ":" + client.hostname() + " 464 " + client.nickname() + " :Password incorrect\r\n";
-	// 	client.queueSend(error_msg);
-	// }
+	if (server.checkPassword(params()[1]))
+	{
+		client.setRegistered(true);
+		std::cout << "success " << std::endl;
+	}
+	else
+	{
+		std::string error_msg = ":" + client.hostname() + " 464 " + client.nickname() + " :Password incorrect\r\n";
+		client.appendToSendBuffer(error_msg);
+	}
 	return;
 }
