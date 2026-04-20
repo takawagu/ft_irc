e24090546e8abc18c2e9e1d3bc23927959198893
s2108tomoka <s2108.ochitomoka@gmail.com>
2026-04-20 16:28:28 +0900

    test用のmain関数を追加

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -3,35 +3,47 @@
 
 #include <iostream>
 
-// int main(int argc, char** argv)
-// {
-//     if (argc != 3) 
-//     {
-//         std::cerr << "Usage: ircserv <port> <password>" << std::endl;
-//         return 1;
-//     }
+int main(int argc, char** argv)
+{
+    if (argc != 3) 
+    {
+        std::cerr << "Usage: ircserv <port> <password>" << std::endl;
+        return 1;
+    }
 
-// 	try
-// 	{
-// 		Config config(argv[1], argv[2]);
-// 		Server server(config);
-// 		server.run();
-// 	}
-// 	catch (const std::exception& e)
-// 	{
-// 		std::cerr << "Error: " << e.what() << std::endl;
-// 		return 1;
-// 	}
-// 	return 0;
-// }
-
-#include "Pass.hpp"
-
-int main(void){
-	std::cout << "tochi test" << std::endl;
-	ACommand* cmd = new Pass();
-	Config config("6667", "password");
-	Server server(config);
-	Client* client = nullptr;
-	cmd->execute(server,*client,0,"Pass password\r\n");
+	try
+	{
+		Config config(argv[1], argv[2]);
+		Server server(config);
+		server.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
 }
+
+// #include "Pass.hpp"
+// #include "Client.hpp"
+
+// int main(void){
+// 	std::cout << "tochi test" << std::endl;
+// 	ACommand* cmd = new Pass();
+// 	Config config("6667", "password");
+// 	Server server(config);
+// 	Client* client = new Client(0,"tochi");
+
+// 	cmd->execute(server,*client,0,"Pass password\r\n");
+// 	std::cout << client->sendBuffer() << std::endl;
+// 	client->removeSentData(client->sendBuffer().length());
+
+// 	cmd->execute(server,*client,0,"Pass \r\n");
+// 	std::cout << client->sendBuffer() << std::endl;
+// 	client->removeSentData(client->sendBuffer().length());
+	
+// 	cmd->execute(server,*client,0,"Pass pass word\r\n");
+// 	std::cout << client->sendBuffer() << std::endl;
+// 	client->removeSentData(client->sendBuffer().length());
+// }
