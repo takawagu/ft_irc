cde8c837eb65283741f2a12831d8258f583915bb
s2108tomoka <s2108.ochitomoka@gmail.com>
2026-04-22 23:16:43 +0900

    scandanavianの小文字に変換する関数を作成

--- srcs/Server/ServerUtils.cpp
+++ srcs/Server/ServerUtils.cpp
@@ -6,22 +6,22 @@ bool Server::checkPassword(const std::string& pass) const
     return pass == _config.password();
 }
 
-std::string changeBrackets(const std::string& nick);
+std::string toScandanavianLower(const std::string& nick);
 
 bool Server::isNicknameTaken(const std::string& nick) const
 {
     std::map<int, Client*>::const_iterator it = _clients.begin();
     for (; it != _clients.end(); it++)
     {
-        std::string client_nick = changeBrackets(it->second->nickname());
-        std::string new_nick=changeBrackets(nick);
+        std::string client_nick = toScandanavianLower(it->second->nickname());
+        std::string new_nick=toScandanavianLower(nick);
         if (client_nick == new_nick)
             return true;
     }
     return false;
 }
 
-std::string changeBrackets(const std::string& nick)
+std::string toScandanavianLower(const std::string& nick)
 {
     std::string new_nick=nick;
     for (size_t i = 0; i < new_nick.length(); i++)
@@ -30,6 +30,7 @@ std::string changeBrackets(const std::string& nick)
             new_nick[i] = '[';
         else if (new_nick[i] == '}')
             new_nick[i] = ']';
+        new_nick[i] = tolower(new_nick[i]);
     }
     return new_nick;
 }
