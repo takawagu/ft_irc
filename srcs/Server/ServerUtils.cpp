251fb82dfd570ca685cef8c90a83f8c359a08873
s2108tomoka <s2108.ochitomoka@gmail.com>
2026-04-22 23:11:58 +0900

    {}と[]は同じとする

--- srcs/Server/ServerUtils.cpp
+++ srcs/Server/ServerUtils.cpp
@@ -6,13 +6,30 @@ bool Server::checkPassword(const std::string& pass) const
     return pass == _config.password();
 }
 
+std::string changeBrackets(const std::string& nick);
+
 bool Server::isNicknameTaken(const std::string& nick) const
 {
     std::map<int, Client*>::const_iterator it = _clients.begin();
     for (; it != _clients.end(); it++)
     {
-        if (it->second->nickname() == nick)
+        std::string client_nick = changeBrackets(it->second->nickname());
+        std::string new_nick=changeBrackets(nick);
+        if (client_nick == new_nick)
             return true;
     }
     return false;
+}
+
+std::string changeBrackets(const std::string& nick)
+{
+    std::string new_nick=nick;
+    for (size_t i = 0; i < new_nick.length(); i++)
+    {
+        if (new_nick[i] == '{')
+            new_nick[i] = '[';
+        else if (new_nick[i] == '}')
+            new_nick[i] = ']';
+    }
+    return new_nick;
 }
