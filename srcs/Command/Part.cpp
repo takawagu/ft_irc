3d689ca926f3027ef5c6adb579c04d7379af43c8
s2108tomoka <s2108.ochitomoka@gmail.com>
2026-04-26 14:16:45 +0900

    add Part command

--- srcs/Command/Part.cpp
+++ srcs/Command/Part.cpp
@@ -1,11 +1,27 @@
 #include "Part.hpp"
 #include "Server.hpp"
 #include "Client.hpp"
+#include "Channel.hpp"
 
 void Part::executeAction(Server& server, Client& client, int fd)
 {
-	(void)server;
-	(void)client;
-	(void)fd;
-	// TODO: implement PART
+	if (params().empty() || params()[0][0] == ':')
+	{
+		server.sendError(client, fd, "461", "PART :Not enough parameters");
+		return;
+	}
+	Channel* channel = server.findChannel(params()[0]);
+	if (channel == NULL)
+	{
+		server.sendError(client, fd, "403", params()[0] + " :No such channel");
+		return;	
+	}
+	std::string partMsg;
+	if (params().size() == 2)
+		partMsg = ":" + client.prefix() + " PART " + channel->name();
+	else
+		partMsg = ":" + client.prefix() + " PART " + channel->name() + " :" + params()[2];
+	server.broadcastToChannel(channel, partMsg);
+	channel->removeMember(&client);
+	server.deleteChannelIfEmpty(channel->name());
 }
